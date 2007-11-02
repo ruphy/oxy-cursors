264b609809583f01586ff9dccfc9f29843e10097
Riccardo Iaconelli <riccardo@kde.org>
2007-11-02 20:45:55 +0100

    First try to have a script that automatically creates a package of cursors ready to be installed

---
+++ package_cursors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Running generate_WORLD.sh..."
+
+cd svgs
+./generate_WORLD.sh
+cd ..
+
+echo
+echo
+echo "Packaging all the cursors"
+echo
+echo
+
+colors="blue yellow brown grey green violet red purple navy sea_blue emerald hot_orange white"
+
+mkdir -p temp
+cd temp
+
+for color in $colors; do
+
+  mkdir -p $color
+  cd $color
+    
+  cat > index.theme << EOF
+[Icon Theme]
+Name = Oxygen $color
+Comment = The Oxygen mouse theme. Oxygenize your desktop!
+EOF
+
+  mkdir cursors
+  cp ../../cursors/$color/* cursors/
+
+  cd ..
+  tar -cf oxygen-cursors-$color.tar.bz2 $color
+
+done
+
+cd ..
+cp temp/*.tar.bz2 .
+rm -rf temp/
